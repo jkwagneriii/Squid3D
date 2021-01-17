e2f0b54a59b178f2156136d89cb06faf35bad4ad
Jake Wagner <jkwagneriii@gmail.com>
2021-01-17 14:25:22 -0600

    got some effects to work on text

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react'
+import './App.scss';
+import { Canvas, useFrame } from 'react-three-fiber'
+import * as THREE from 'three';
+
+import { softShadows, MeshWobbleMaterial, OrbitControls } from "drei";
+
+import { useSpring, a} from "react-spring/three"
+
+import Terror from './Gendry_Regular.json'
+
+softShadows();
+
+const SpinningText = ({position, args, color, speed}) => {
+  const mesh = useRef(null);
+
+  const font = new THREE.FontLoader().parse(Terror);
+
+  // configure font geometry
+  const textOptions = {
+    font,
+    size: 4,
+    height: 1
+  };
+
+  useFrame(() => (mesh.current.rotation.x =+  0.01 ))
+
+  const [expand, setExpand] = useState(false)
+
+  const props = useSpring({
+    scale: expand ? [1.6,1.6,1.6] : [1,1,1]
+  })
+
+  return (
+    <a.mesh 
+    onClick={() => setExpand(!expand)} 
+    scale={props.scale}
+    castShadow 
+    position={position} 
+    ref={mesh}>
+      <textGeometry attach='geometry' args={[args, textOptions]} />
+      <MeshWobbleMaterial 
+      attach='material' 
+      color={color} 
+      speed={speed} 
+      factor={0.1}
+      roughness={0.1}
+      metalness={0.5} />
+    </a.mesh>
+  )
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas shadowMap colorManagement camera={{position: [5, 2, 20], fov: 80}}>
+        <ambientLight intensity={0.3}/>
+        <directionalLight 
+        castShadow
+        position={[0, 10, 0]} 
+        intensity={3}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-far={50}
+        shadow-camera-left={-10}
+        shadow-camera-right={10}
+        shadow-camera-top={10}
+        shadow-camera-bottom={-10}
+        />
+        <pointLight position={[-10, 0, -20]} intensity={0.5}/>
+        <pointLight position={[0, -10, 0]} intensity={1.5}/>
+
+        <group>
+          <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
+            <planeBufferGeometry attach="geometry" args={[100, 100]}/>
+            <shadowMaterial attach="material" opacity={0.3} />
+          </mesh>
+        <SpinningText args={'Squid'} position={[-5, 3, 0]} color="red" speed={3}/>
+        <SpinningText args={'Swim'} position={[-1, -1, 3]} color="red" speed={3}/>
+        </group>
+
+        <group>
+        </group>
+
+
+        <OrbitControls />
+      </Canvas>
+    </>
   );
 }
 
